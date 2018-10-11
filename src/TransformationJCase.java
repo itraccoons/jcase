07a3b416e764caefd89db02c199c0217a1629ffb
raccoon1 <raccoon1@doe.raccoons.co>
2018-10-11 17:03:12 +0300

    change indentation and update files

--- src/TransformationJCase.java
+++ src/TransformationJCase.java
@@ -2,7 +2,7 @@
 )
 
 public class TransformationJCase {
-    public static void main(String[] args) {
-        System.out.println("Ready.Steady.Go");
-    }
+  public static void main(String[] args) {
+    System.out.println("Ready.Steady.Go");
+  }
 }
