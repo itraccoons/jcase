50367c5b06f25488a1ec70d51166950befe3f1ae
raccoon1 <raccoon1@doe.raccoons.co>
2018-10-15 09:13:48 +0300

    add src dir

---
+++ src/com/raccoons/transformationjcase/TransformationJCase.java
@@ -0,0 +1,10 @@
+package com.raccoons.transformationjcase;
+
+@SuppressWarnings("InstanceMethodNamingConvention"
+)
+
+public class TransformationJCase {
+  public static void main(String[] args) {
+    System.out.println("Ready.Steady.Go");
+  }
+}
