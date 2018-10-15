9cc29390e6d194db884bc76f545ca9dbba7c9b83
raccoon1 <raccoon1@doe.raccoons.co>
2018-10-15 15:45:12 +0300

    add files

---
+++ src/main/java/org/raccoons/backyards/TransformationJCase.java
@@ -0,0 +1,10 @@
+package main.java.org.raccoons.backyards;
+
+@SuppressWarnings("InstanceMethodNamingConvention"
+)
+
+public class TransformationJCase {
+  public static void main(String[] args) {
+    System.out.println("Ready.Steady.Go");
+  }
+}
