dbbad03b7be6e018b23045e57a24cac85c4b3dd9
raccoon1 <raccoon1@doe.raccoons.co>
2018-10-22 15:39:28 +0300

    add ktlint to Makefile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,29 @@
 plugins {
-  java
-  application
-  checkstyle
-  id("com.google.osdetector") version "1.6.0"
+    java
+    application
+    checkstyle
+    id("com.google.osdetector") version "1.6.0"
 }
 
 repositories {
-  mavenCentral()
-  jcenter()
+    mavenCentral()
+    jcenter()
 }
 
 checkstyle {
-  toolVersion = "8.13"
+    toolVersion = "8.13"
 }
 
 tasks.create("os") {
-  doLast {
-    println("osdetector.os: " + osdetector.os)
-    println("osdetector.arch: " + osdetector.arch)
-    println("osdetector.release: " + osdetector.release)
-  }
+    doLast {
+        println("osdetector.os: " + osdetector.os)
+        println("osdetector.arch: " + osdetector.arch)
+        println("osdetector.release: " + osdetector.release)
+    }
 }
 
 version = "0.1.0"
 
 application {
-  mainClassName = "org.raccoons.backyards.TransformationJCase"
+    mainClassName = "org.raccoons.backyards.TransformationJCase"
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 pluginManagement {
-  repositories {
-    mavenCentral()
-    gradlePluginPortal()
-  }
-
-  resolutionStrategy {
-    eachPlugin {
-      if(requested.id.namespace == "com.google" && requested.id.name == "osdetector") {
-        useModule("com.google.gradle:osdetector-gradle-plugin:1.6.0")
-      }
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
     }
-  }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.google" && requested.id.name == "osdetector") {
+                useModule("com.google.gradle:osdetector-gradle-plugin:1.6.0")
+            }
+        }
+    }
 }
 
 rootProject.name = "jcase"
