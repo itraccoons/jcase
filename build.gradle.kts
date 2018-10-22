f448754db7b534895fbab3515549bda57b42c35a
raccoon1 <raccoon1@doe.raccoons.co>
2018-10-22 14:00:56 +0300

    Replace Groovy with Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+  java
+  kotlin("jvm") version "1.2.71"
+  application
+  checkstyle
+  id("com.google.osdetector") version "1.6.0"
+}
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+checkstyle {
+  toolVersion = "8.13"
+}
+
+tasks.create("os") {
+  doLast {
+    println("osdetector.os: " + osdetector.os)
+    println("osdetector.arch: " + osdetector.arch)
+    println("osdetector.release: " + osdetector.release)
+  }
+}
+
+version = "0.1.0"
+
+application {
+  mainClassName = "org.raccoons.backyards.TransformationJCase"
+}
+
