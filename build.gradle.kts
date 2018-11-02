11881b2987df9f0cc4d48e08d40226871180cda6
raccoon1 <raccoon1@doe.raccoons.co>
2018-11-02 16:04:00 +0200

    add comments to gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,27 @@
+/*
+ * Gradle Build with Kotlin DSL
+ *
+ * Written by: Oleksii Kucheruk <ok@raccoons.co> on Fri Oct 12 2018
+ */
+
 plugins {
     java
-    jacoco
+    jacoco // Java Code Coverage Library
     application
-    checkstyle
+    checkstyle // Static code analysis for Java source code
     `project-report`
     `build-dashboard`
 }
 
+/*
+ * Create read-only ext variables
+ */
 val junitApiVersion by extra { "5.3.1" }
 val jacocoVersion by extra { "0.8.2" }
 val checkstyleVersion by extra { "8.13" }
+val checkstyleConfigFile by extra { "config/checkstyle/google_checks.xml" }
+val minimumBundleCoverage by extra { 0.6 }
+val minimumClassCoverage by extra { 0.8 }
 
 dependencies {
     testCompile("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
@@ -23,6 +35,7 @@ jacoco {
 
 checkstyle {
     toolVersion = checkstyleVersion
+    configFile = rootProject.file(checkstyleConfigFile)
 }
 
 allprojects {
@@ -35,19 +48,52 @@ allprojects {
     }
 }
 
+application {
+    mainClassName = "org.raccoons.backyards.TransformationJCase"
+}
+
+/*
+ * Enable required Checkstyle reports formats (HTML, XML).
+ * Note: build-dashboard plugin depends on these setting
+ */
+tasks.withType<Checkstyle> {
+    reports.html.isEnabled = true
+    reports.xml.isEnabled = false
+    /*
+     * reports{
+     *   html.isEnabled = true
+     *   xml.isEnabled = false
+     * }
+     */
+}
+
+/*
+ * Enable required JaCoCo reports formats (HTML, XML, CSV).
+ * Note: build-dashboard plugin depends on these setting
+ */
+val jacocoTestReport by tasks.getting(JacocoReport::class) {
+    reports.html.isEnabled = true
+    reports.xml.isEnabled = false
+    reports.csv.isEnabled = false
+}
+
+/*
+ * Set Code Coverage minimum limits for Bundle and Classes.
+ * Exclude main class
+ */
 val jacocoTestCoverageVerification by tasks.getting(JacocoCoverageVerification::class) {
     violationRules {
         rule {
             element = "BUNDLE"
             limit {
-                minimum = BigDecimal.valueOf(0.6)
+                minimum = BigDecimal.valueOf(minimumBundleCoverage)
             }
         }
         rule {
             element = "CLASS"
             excludes = listOf("*TransformationJCase")
             limit {
-                minimum = BigDecimal.valueOf(0.8)
+                minimum = BigDecimal.valueOf(minimumClassCoverage)
             }
         }
     }
@@ -55,15 +101,11 @@ val jacocoTestCoverageVerification by tasks.getting(JacocoCoverageVerification::
     check.dependsOn(this)
 }
 
-val jacocoTestReport by tasks.getting(JacocoReport::class) {
-    reports.html.isEnabled = true
-}
-
+/*
+ * Enable native support for JUnit.
+ * Add to test task jacocoTestReport execution
+ */
 val test by tasks.getting(Test::class) {
     useJUnitPlatform()
     finalizedBy(jacocoTestReport)
-}
-
-application {
-    mainClassName = "org.raccoons.backyards.TransformationJCase"
 }
