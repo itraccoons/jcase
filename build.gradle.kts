65e0eb71cc0bd17f5ce6d668bf553148b8ca537e
raccoon1 <raccoon1@doe.raccoons.co>
2018-11-28 21:53:01 +0200

    fix ReferenceEquality

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,20 @@
  * Written by: Oleksii Kucheruk <ok@raccoons.co> on Fri Oct 12 2018
  */
 
+/*
+ * Create read-only ext variables
+ */
+val packageName by extra { "org.raccoons.backyards" }
+val mainClass by extra { "TransformationJCase" }
+val checkstyleVersion by extra { "8.13" }
+val checkstyleConfigFile by extra { "config/checkstyle/google_checks.xml" }
+val junitApiVersion by extra { "5.3.1" }
+val jacocoVersion by extra { "0.8.2" }
+val minimumBundleCoverage by extra { 0.5 }
+val minimumClassCoverage by extra { 0.5 }
+val springbootVersion by extra { "2.1.0.RELEASE" }
+var errorproneVersion by extra { "2.3.2" }
+
 plugins {
     java
     application
@@ -39,79 +53,53 @@ allprojects {
             from("checkerframework.gradle")
         }
     }
-}
-
-/*
- * Create read-only ext variables
- */
-val packageName by extra { "org.raccoons.backyards" }
-val mainClass by extra { "TransformationJCase" }
-val checkstyleVersion by extra { "8.13" }
-val checkstyleConfigFile by extra { "config/checkstyle/google_checks.xml" }
-val junitApiVersion by extra { "5.3.1" }
-val jacocoVersion by extra { "0.8.2" }
-val minimumBundleCoverage by extra { 0.5 }
-val minimumClassCoverage by extra { 0.5 }
-
-dependencies {
-    // implementation("com.google.guava:guava:27.0-jre")
-    // implementation("com.google.http-client:google-http-client:1.27.0")
-    // implementation("org.apache.httpcomponents:httpclient:4.5.6")
-    implementation("com.google.cloud.tools:jib-core:0.1.0")
-    testCompile("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
-    testImplementation("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:" + junitApiVersion)
-    implementation("org.springframework.boot:spring-boot-starter:2.1.0.RELEASE")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.1.0.RELEASE")
-    errorprone("com.google.errorprone:error_prone_core:2.3.2")
-    errorproneJavac("com.google.errorprone:error_prone_core:2.3.2")
-}
-application {
-    mainClassName = packageName + "." + mainClass
-}
-
-checkstyle {
-    toolVersion = checkstyleVersion
-    configFile = rootProject.file(checkstyleConfigFile)
-}
-
-jacoco {
-    toolVersion = jacocoVersion
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", System.getenv("CIRCLE_PROJECT_USERNAME") + "_" + System.getenv("CIRCLE_PROJECT_REPONAME"))
-        property("sonar.organization", System.getenv("CIRCLE_PROJECT_USERNAME") + "-github")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.login", System.getenv("SONARCLOUD_TOKEN"))
-        property("sonar.branch.name", System.getenv("CIRCLE_BRANCH"))
-        // property("sonar.branch.target", "master") // commented because the main(master) branch must not have a target
-        property("sonar.java.checkstyle.reportPaths", "build/reports/checkstyle/main.xml,build/reports/checkstyle/test.xml")
+    application {
+        mainClassName = packageName + "." + mainClass
     }
-}
 
-jib {
+    checkstyle {
+        toolVersion = checkstyleVersion
+        configFile = rootProject.file(checkstyleConfigFile)
+    }
+
+    jacoco {
+        toolVersion = jacocoVersion
+    }
+
+    sonarqube {
+        properties {
+            property("sonar.projectKey", System.getenv("CIRCLE_PROJECT_USERNAME") + "_" + System.getenv("CIRCLE_PROJECT_REPONAME"))
+            property("sonar.organization", System.getenv("CIRCLE_PROJECT_USERNAME") + "-github")
+            property("sonar.host.url", "https://sonarcloud.io")
+            property("sonar.login", System.getenv("SONARCLOUD_TOKEN"))
+            property("sonar.branch.name", System.getenv("CIRCLE_BRANCH"))
+            // property("sonar.branch.target", "master") // commented because the main(master) branch must not have a target
+            property("sonar.java.checkstyle.reportPaths", "build/reports/checkstyle/main.xml,build/reports/checkstyle/test.xml")
+        }
+    }
+
+    jib {
+        /*
+        from{
+            image = "circleci/openjdk:11-jdk"
+        }
+        */
+        to {
+            image = "gcr.io/td-jcase/jcase:" + project.version
+            credHelper = "gcr"
+        }
+    }
+
     /*
-    from{
-        image = "circleci/openjdk:11-jdk"
+    gretty {
+        httpEnabled = true
+        httpPort = 8080
+        httpsEnabled = true
+        // httpsPort = 443
+        servletContainer = "jetty9"
     }
     */
-    to {
-        image = "gcr.io/td-jcase/jcase:" + project.version
-        credHelper = "gcr"
-    }
 }
-
-/*
-gretty {
-    httpEnabled = true
-    httpPort = 8080
-    httpsEnabled = true
-    // httpsPort = 443
-    servletContainer = "jetty9"
-}
- */
 
 /*
  * Enable required Checkstyle reports formats (HTML, XML).
@@ -170,4 +158,18 @@ val jacocoTestCoverageVerification by tasks.getting(JacocoCoverageVerification::
 val test by tasks.getting(Test::class) {
     useJUnitPlatform()
     finalizedBy(jacocoTestReport)
+}
+
+dependencies {
+    implementation("com.google.guava:guava:27.0-jre")
+    // implementation("com.google.http-client:google-http-client:1.27.0")
+    // implementation("org.apache.httpcomponents:httpclient:4.5.6")
+    implementation("com.google.cloud.tools:jib-core:0.1.0")
+    testCompile("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:" + junitApiVersion)
+    implementation("org.springframework.boot:spring-boot-starter:" + springbootVersion)
+    implementation("org.springframework.boot:spring-boot-starter-web:" + springbootVersion)
+    errorprone("com.google.errorprone:error_prone_core:" + errorproneVersion)
+    errorproneJavac("com.google.errorprone:error_prone_core:" + errorproneVersion)
 }
